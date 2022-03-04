50b4aba66fe5f3ee9c7ca14b1c729f0cb777ee0f
jooo0922 <2014102458@khu.ac.kr>
2022-03-05 00:30:26 +0900

    add: Init app

---
+++ src/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import "./style.css";
+
+class App {
+  constructor() {}
+
+  resize() {}
+
+  animate() {}
+}
+
+window.onload = () => {
+  new App();
+};
