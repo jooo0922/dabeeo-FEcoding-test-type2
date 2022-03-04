8d8dffd52664156ed1b45b25f0a3c26912209b84
jooo0922 <2014102458@khu.ac.kr>
2022-03-05 02:23:07 +0900

    add: Add func returns random pos

---
+++ src/utils.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// 랜덤한 좌표값의 범위를 -20 ~ 20으로 잡아준 이유는
+// Plane(지도)의 사이즈가 40 * 40 이기 때문에
+// 그 안에 들어오려면 -20 ~ 20 사이의 좌표값이 필요하다고 생각했습니다.
+const MIN_POS = -20;
+const MAX_POS = 20;
+
+// 마커들의 x, z 좌표값을 랜덤하게 리턴해주는 함수
+function randomPos() {
+  let pos = [];
+
+  for (let i = 0; i < 2; i++) {
+    const coord = Math.random() * (MAX_POS - MIN_POS) + MIN_POS;
+    pos.push(coord);
+  }
+
+  return pos;
+}
+
+export default randomPos;
